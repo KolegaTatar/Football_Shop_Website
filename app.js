b476da03a6b668effec2528b8633e65429a4146f
Wiktor <wiktor.tatarynowicz@gmail.com>
2023-06-29 14:13:56 +0200

    shop update

---
+++ app.js
@@ -0,0 +1,80 @@
+function adidasButy(){
+    var tresc_div = "";
+    for(i=2;i<15;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-15%</span><div class="image"><img src="zdjęcia/buty'+i+'.png" alt="adidas-korki"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>adidas Predator Edge .1 FG</h3><div class="cena"> 688,99zł <span>810.99zł</span> </div></div></div>'
+    }
+    document.getElementById("adidasButy").innerHTML = tresc_div;
+};
+
+function adidasPilka(){
+    var tresc_div = "";
+    for(i=1;i<8;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-5%</span><div class="image"><img src="zdjęcia/piłka'+i+'.png" alt="adidas-piłka"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>Adidas Rihla Pro</h3><div class="cena"> 285zł <span>300zł</span> </div></div></div>'
+    }
+    document.getElementById("adidasPilka").innerHTML = tresc_div;
+};
+
+function adidasOchraniacze(){
+    var tresc_div = "";
+    for(i=1;i<5;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-5%</span><div class="image"><img src="zdjęcia/ochraniacze'+i+'.png" alt="adidas-ochraniacze"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>Adidas X League Shin Guards</h3><div class="cena"> 285zł <span>300zł</span> </div></div></div>'
+    }
+    document.getElementById("adidasOchraniacze").innerHTML = tresc_div;
+};
+
+function nikeButy(){
+    var tresc_div = "";
+    for(i=21;i<37;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-15%</span><div class="image"><img src="zdjęcia/buty'+i+'.png" alt="nike-korki"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>Nike Mercurial Superfly 8 Elite Fg</h3><div class="cena"> 688,99zł <span>810.99zł</span> </div></div></div>'
+    }
+    document.getElementById("nikeButy").innerHTML = tresc_div;
+};
+
+function nikePilka(){
+    var tresc_div = "";
+    for(i=19;i<26;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-5%</span><div class="image"><img src="zdjęcia/piłka'+i+'.png" alt="nike-piłka"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>nike premier league flight third</h3><div class="cena"> 285zł <span>300zł</span> </div></div></div>'
+    }
+    document.getElementById("nikePilka").innerHTML = tresc_div;
+};
+
+function nikeOchraniacze(){
+    var tresc_div = "";
+    for(i=5;i<8;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-5%</span><div class="image"><img src="zdjęcia/ochraniacze'+i+'.png" alt="nike-ochraniacze"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>Nike Mercurial Lite Guards</h3><div class="cena"> 285zł <span>300zł</span> </div></div></div>'
+    }
+    document.getElementById("nikeOchraniacze").innerHTML = tresc_div;
+};
+
+function NBButy(){
+    var tresc_div = "";
+    for(i=37;i<46;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-15%</span><div class="image"><img src="zdjęcia/buty'+i+'.png" alt="nb-korki"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>New balance tekela 3+ magique fg</h3><div class="cena"> 688,99zł <span>810.99zł</span> </div></div></div>'
+    }
+    document.getElementById("NBButy").innerHTML = tresc_div;
+};
+
+function pumaButy(){
+    var tresc_div = "";
+    for(i=46;i<55;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-15%</span><div class="image"><img src="zdjęcia/buty'+i+'.png" alt="puma-korki"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>Puma Ultra 2.1 FG/AG</h3><div class="cena"> 688,99zł <span>810.99zł</span> </div></div></div>'
+    }
+    document.getElementById("pumaButy").innerHTML = tresc_div;
+};
+
+function pumaPilka(){
+    var tresc_div = "";
+    for(i=26;i<33;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-5%</span><div class="image"><img src="zdjęcia/piłka'+i+'.png" alt="puma-piłka"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>Puma Cage Ballw Alert</h3><div class="cena"> 285zł <span>300zł</span> </div></div></div>'
+    }
+    document.getElementById("pumaPilka").innerHTML = tresc_div;
+};
+
+function PumaOchraniacze(){
+    var tresc_div = "";
+    for(i=8;i<11;i++){
+        tresc_div = tresc_div + '<div class="box"><span class="przecena">-5%</span><div class="image"><img src="zdjęcia/ochraniacze'+i+'.png" alt="puma-ochraniacze"><div class="ikony"><a href="#" class="fas fa-heart"></a><a href="#" class="cart-btn">do koszyka</a><a href="#" class="fas fa-share"></a></div></div><div class="zawartosc"><h3>Puma Ultra Twist Sleeve</h3><div class="cena"> 285zł <span>300zł</span> </div></div></div>'
+    }
+    document.getElementById("PumaOchraniacze").innerHTML = tresc_div;
+};
+
